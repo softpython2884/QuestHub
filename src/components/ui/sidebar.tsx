18e3fce58d30eceaf3704b3099570f5d628dc6b9
Enzo prados <enzo.prados@gmail.com>
2025-06-25 08:04:38 +0000

    Console Error
    Hydration failed because the server rendered HTML didn't

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -314,7 +314,7 @@ const SidebarInset = React.forwardRef<
     <div
       ref={ref}
       className={cn(
-        "relative flex min-h-svh flex-1 flex-col bg-transparent", // Removed w-full, rely on flex-1
+        "relative flex min-h-svh w-full flex-1 flex-col bg-background",
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
@@ -639,9 +639,13 @@ const SidebarMenuSkeleton = React.forwardRef<
     showIcon?: boolean
   }
 >(({ className, showIcon = false, ...props }, ref) => {
-  const width = React.useMemo(() => {
-    return `${Math.floor(Math.random() * 40) + 50}%`
-  }, [])
+  const [width, setWidth] = React.useState("75%");
+
+  React.useEffect(() => {
+    // This will only run on the client, after hydration, preventing the mismatch
+    setWidth(`${Math.floor(Math.random() * 40) + 50}%`);
+  }, []);
+
 
   return (
     <div
