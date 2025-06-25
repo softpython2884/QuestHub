0a4b124826b7e348a1b6dcc0f81190a67f45cb34
Enzo prados <enzo.prados@gmail.com>
2025-06-25 09:03:11 +0000

    alors la on a un pb, genre le truc avec le fond noir je crois qu'il c re

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -314,7 +314,7 @@ const SidebarInset = React.forwardRef<
     <div
       ref={ref}
       className={cn(
-        "relative flex min-h-svh w-full flex-1 flex-col bg-background",
+        "relative flex min-h-svh w-full flex-1 flex-col bg-transparent",
         "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:m-2 md:peer-data-[state=collapsed]:peer-data-[variant=inset]:ml-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         className
       )}
